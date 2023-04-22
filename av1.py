e8b44ee8d03313984312a301d0a1252b0ed1c1d0
Tiago Moura <129202390+TIAGOMOURADS@users.noreply.github.com>
2023-04-22 09:37:04 -0300

    Add files via upload

---
+++ av1.py
@@ -0,0 +1,7 @@
+#1) Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de
+#três e que se encontram no conjunto dos números de 1 até 500.
+soma = 0
+for i in range(1, 501):
+    if i % 2 != 0 and i % 3 == 0:
+        s += i
+        print(s)
